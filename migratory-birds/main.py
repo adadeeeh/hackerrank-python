94fd26d8538f17db2acb40fb1f547ea7f3a8234e
Faturrahman Ma'ruf <faturrahmanmakruf@gmail.com>
2022-06-21 14:44:03 +0700

    solved migratory birds

---
+++ migratory-birds/main.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+def migratoryBirds(arr):
+    birdCount = [0, 0, 0, 0, 0, 0]
+
+    for bird in arr:
+        birdCount[bird] += 1
+
+    return birdCount.index(max(birdCount))
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+
+    arr = list(map(int, input().strip().split()))
+
+    result = migratoryBirds(arr)
+
+    print(result)
